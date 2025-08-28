b7e3b1ac5c6d26a1d34872666a2a1a33cac8d5f5
Soham Goswami <sohamgoswami07@gmail.com>
2025-08-28 14:14:47 +0530

    Updates

--- Constants/slidesData.js
+++ Constants/slidesData.js
@@ -2,42 +2,42 @@ const slides = [
     {
         title: "着物",
         description: "着物は、日本の伝統的な衣服で、長い袖と帯が特徴です。季節や場面に応じた柄や素材が選ばれ、格式ある場やお祝い事などで着用されます。結婚式、成人式、茶道などでよく見られ、美しい装いとして世界中でも高く評価されています。",
-        imageUrl: "/Background/kimono.jpg"
+        imageUrl: "../public/Background/kimono.jpg"
     },
     {
         title: "侍",
         description: "侍は、日本の武士階級の一員で、主君に仕え、武道や礼儀を重んじました。名誉を重視し、時には命をかけて忠義を尽くしました。江戸時代まで大きな影響力を持ちました。",
-        imageUrl: "/Background/samurai.jpg"
+        imageUrl: "../public/Background/samurai.jpg"
     },
     {
         title: "生け花",
         description: "生け花は、日本の伝統的な花の芸術で、花や枝を使って自然の美と調和を表現します。形や空間を大切にし、静けさや季節感を演出します。精神的な修練としても親しまれています。",
-        imageUrl: "/Background/ikebana.jpg"
+        imageUrl: "../public/Background/ikebana.jpg"
     },
     {
         title: "料理",
         description: "料理は、食材を調理して作る食事や芸術です。日本料理は、季節感、美しい盛り付け、出汁の風味を重視します。地域ごとに多様な特色があり、健康的で繊細な味わいが特徴です。",
-        imageUrl: "/Background/cuisine.jpg"
+        imageUrl: "../public/Background/cuisine.jpg"
     },
     {
         title: "歌舞伎",
         description: "歌舞伎は、日本の伝統的な演劇で、豪華な衣装、独特な化粧、大げさな演技が特徴です。物語は歴史や恋愛など多彩で、音楽や舞踊も融合しています。江戸時代から続く大衆芸能です。",
-        imageUrl: "/Background/kabuki.jpg"
+        imageUrl: "../public/Background/kabuki.jpg"
     },
     {
         title: "書道",
         description: "書道は、筆と墨を使って文字を美しく書く日本の伝統芸術です。文字の形、流れ、余白を大切にし、心を落ち着ける精神修行としても親しまれています。芸術と表現の融合です。",
-        imageUrl: "/Background/calligraphy.jpg"
+        imageUrl: "../public/Background/calligraphy.jpg"
     },
     {
         title: "茶道",
         description: "茶道は、日本の伝統的なお茶の儀式で、抹茶を点てて客人をもてなします。礼儀作法、美しい所作、静けさを重んじ、精神的な修養や和の心を体現する文化です。",
-        imageUrl: "/Background/tea.jpg"
+        imageUrl: "../public/Background/tea.jpg"
     },
     {
         title: "仏教",
         description: "仏教は、インドで始まり日本に伝わった宗教で、苦しみからの解脱を目指します。慈悲や悟りの教えを重視し、日本文化や芸術、日常生活にも深い影響を与えています。",
-        imageUrl: "/Background/buddhism.jpg"
+        imageUrl: "../public/Background/buddhism.jpg"
     },
 ]
 
